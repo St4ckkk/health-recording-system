10048145e41540d2a3f86d37597449bc8835c160
St4ckkk <keyanandydelgado@gmail.com>
2025-03-11 14:26:07 +0800

    haha

---
+++ frontend/views/receptionist/components/header.php
@@ -0,0 +1,12 @@
+<header class="p-5 flex justify-end items-center border-b border-gray-200">
+    <div class="flex items-center gap-2">
+        <i class='bx bx-bell text-gray-900' style="font-size: 1.4rem;"></i>
+        <div class="avatar w-12 h-12">
+            <i class="bx bx-user text-3xl"></i>
+        </div>
+        <div>
+            <div class="text-gray-900 font-medium">John Doe</div>
+            <div class="text-gray-400 text-xs">Patient</div>
+        </div>
+    </div>
+</header>
---
+++ frontend/views/receptionist/components/sidebar.php
@@ -0,0 +1,27 @@
+<nav class="bg-white p-4 sidebar border border-gray-200">
+    <div class="logo-container text-white p-4 mb-6">
+        <div class="mr-3">
+            <div class="logo">
+                <div class="w-12 h-12 flex justify-center items-center font-bold text-white">TC</div>
+            </div>
+        </div>
+    </div>
+    <div class="sidebar-content">
+        <ul class="space-y-md">
+            <li class="px-sm py-2 rounded-md hover:bg-primary-light flex items-center text-gray-900">
+                <i class='bx bx-home text-lg mr-2'></i> Dashboard
+            </li>
+            <li class="px-sm py-2 rounded-md bg-primary-light flex items-center text-gray-900">
+                <i class='bx bx-calendar text-lg mr-2'></i> Appointments
+            </li>
+            <li class="px-sm py-2 rounded-md hover:bg-primary-light flex items-center text-gray-900">
+                <i class='bx bx-bell text-lg mr-2'></i> Notifications
+            </li>
+        </ul>
+    </div>
+    <div class="logout-container">
+        <div class="px-sm py-2 rounded-md hover:bg-primary-light flex items-center text-gray-900 cursor-pointer">
+            <i class='bx bx-log-out text-lg mr-2'></i> Logout
+        </div>
+    </div>
+</nav>
